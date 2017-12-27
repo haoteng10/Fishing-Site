4607f293239c14235aff36a19a7db6730144ce43
Hao Teng <haoteng10@gmail.com>
2017-12-27 15:18:30 -0500

    Change

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-app.set("view engine", "ejs");
-
-mongoose.connect('mongodb://localhost/my_database', {useMongoClient: true});
-
 var bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var fishingSite = [{
-    name: "Great Grand Park",
-    url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT84UgoxCM4rb51iQm-AxgYKxijqTPoTF-Ge59RtNJRQPnvqVsv"}
-    ];
+mongoose.connect("mongodb://localhost/fishing_site", {useMongoClient: true});
+
+
+// SCHEMA SETUP
+var siteSchema = new mongoose.Schema({
+   name: String,
+   url: String,
+   location: String,
+   description: String
+});
+
+var Site = mongoose.model("Site", siteSchema);
+
+
+// var fishingSite = [{
+//     name: "Great Grand Park",
+//     url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT84UgoxCM4rb51iQm-AxgYKxijqTPoTF-Ge59RtNJRQPnvqVsv"}
+//     ];
 
 app.get("/", function(req,res){
       res.render("landing");
@@ -22,6 +34,22 @@ app.get("/sites", function(req,res){
 });
 
 app.post("/sites", function(req,res){
+    // get data from form and add to fishing sites array
+    var name = req.body.name;
+    var url = req.body.url;
+    var location = req.body.loc;
+    var desc = req.body.description;
+    var newSite = {name: name, url: url, location: location, description: desc}
+    
+    // Create a new campground and save to DB
+    Site.create(newSite, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to the page
+            res.redirect("/sites");
+        }
+    });
     
 });
 
