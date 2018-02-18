041e18c8b6032b34b549bfdf26659a6c2f88faed
Hao Teng <haoteng10@gmail.com>
2018-02-18 22:25:42 +0000

    Edit Comment Route Complete

--- routes/Comments.js
+++ routes/Comments.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router  = express.Router({mergeParams: true});
 var Sites = require("../models/FishingSite");
-var Comment = require("../models/Comment");
+var Comments = require("../models/Comment");
 
+// COMMENT NEW ROUTE
 router.get("/new", function(req, res){
     Sites.findById(req.params.id, function(err, foundSite){
         if (err){
@@ -14,12 +15,13 @@ router.get("/new", function(req, res){
     
 });
 
+// COMMENT ADD ROUTE
 router.post("/", function(req, res){
     Sites.findById(req.params.id, function(err, foundSite){
         if (err) {
             console.log(err);
         } else {
-            Comment.create(req.body.comment, function(err, comment){
+            Comments.create(req.body.comment, function(err, comment){
                 if (err){
                     console.log(err);
                 } else {
@@ -27,9 +29,33 @@ router.post("/", function(req, res){
                     foundSite.save();
                     res.redirect("/sites/" + req.params.id);
                 }
-            })
+            });
         }
-    })
-})
+    });
+});
+
+//COMMENT EDIT ROUTE
+router.get("/:commentId/edit", function(req, res){
+    Comments.findById(req.params.commentId, function(err, foundComment) {
+        if (err){
+            console.log(err);
+        } else {
+            res.render("Comments/edit", {comment: foundComment, site_id: req.params.id});
+        }    
+        
+    });
+    
+});
+
+// COMMENT UPDATE ROUTE
+router.put("/:commentId", function(req, res){
+    Comments.findByIdAndUpdate(req.params.commentId, req.body.comment, function(err, updatedComment){
+        if (err){
+            console.log("Comment Update went wrong!");
+        } else {
+            res.redirect("/sites/" + req.params.id);
+        }
+    });
+});
 
 module.exports = router;
